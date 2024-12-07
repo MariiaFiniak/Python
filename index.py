b3e61cd25e987882ffae1b2c0fae4ac28511f077
Maria <finyakmasha@ukr.net>
2024-12-07 11:33:48 +0100

    delete

--- index.py
+++ index.py
@@ -1,18 +1,55 @@
-tasks = {}
+tasks={}
 def get_id(tasks):
 	if not len(tasks):
-	return 1
-	return max(tasks.keys()) + 1
-def priority():
-            try:
-            while True:
-             user_prioryty= int(input(f'Enter priority 1:low,2:middle,3:hight'))
-            if user_prioryty==1:
-            return "low"
-            elif user_prioryty==2:
-           return 'middle'
-           elif user_prioryty==3:
-           return 'hight'
-           else:"Please enter number 1-3"
-           except ValueError:
-  'Do not correct value'
+		return 1
+	return max(tasks.keys())+1
+	def tasks_to_string(tasks):
+		result=""
+		for task_id,task_values in tasks.items():
+			result+=f'{task_id}:{task_values['title']}|{task_values['desk']}|{task_values['priority']}|{task_value['status']}\n
+			return result
+	def write_task(tasks):
+		with open("tasks.txt","w") as file:
+			file.write(tasks_to_string(tasks))
+			def read_task_from_file(filename):
+			tasks={}
+			try:
+			with open(filename,"r") as files:
+			for line in file:
+			parts=line.strip(split("|"))
+			if len(parts)==5:
+			task_id=int(parts[0])[:-1]
+			tasks[tasl_id]={
+				"title":parts[1],
+				"desk":parts[2],
+				"priority_text":parts[3],
+				"status":parts[4]
+			}
+			except FileNotFoundError:
+			print("Not found file.")
+			return tasks
+		def view_task():
+		try:
+		with open("task.txt","r")	as file:
+		content=file.read()
+		print(content)
+		except FileNotFoundError:
+			print("Not found file.")
+			return content
+			def delete_task():
+			view_task()
+			try:
+			delete_coice=int(input("Please,enter ID:"))
+			if delete_choice in tasks:
+			del task[delete_choice]
+			save_task(task)
+			print(f"Task c ID{delete_choice}delete")
+			else print("Task with ID not find")
+			except ValueError:
+			print("Please,enter correct number.")
+			def 
+		
+
+
+
+	
