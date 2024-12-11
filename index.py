fc76aabc1ad0f1fefe41747da5a292b21f6123af
Maria <finyakmasha@ukr.net>
2024-12-11 13:28:13 +0100

    priority

--- index.py
+++ index.py
@@ -8,10 +8,25 @@ def get_id(tasks):
 		for task_id,task_values in tasks.items():
 			result+=f'{task_id}:{task_values['title']}|{task_values['desk']}|{task_values['priority']}|{task_value['status']}\n
 			return result
+	def priority_tasks():
+	try:
+	while True:
+	priority==int(input("Choose priority task(1-low,2-middle,3-high):"))
+	if priority==1:
+	return "low"
+	elif priority==2:
+	return "middle"
+	elif priority==3:
+	return "high"
+	else:
+	print("Please,enter  a numberfrom 1-3 ")	
+	except ValueError:
+	print("Incorrect number")
+	save_task(priority)	
 	def write_task(tasks):
 		with open("tasks.txt","w") as file:
 			file.write(tasks_to_string(tasks))
-			def read_task_from_file(filename):
+			def read_task(filename):
 			tasks={}
 			try:
 			with open(filename,"r") as files:
@@ -28,6 +43,9 @@ def get_id(tasks):
 			except FileNotFoundError:
 			print("Not found file.")
 			return tasks
+			def sort_task():
+			view_task()
+			sort_priority_status=input("Sort(priority/status):")
 		def view_task():
 		try:
 		with open("task.txt","r")	as file:
@@ -47,7 +65,30 @@ def get_id(tasks):
 			else print("Task with ID not find")
 			except ValueError:
 			print("Please,enter correct number.")
-			def 
+			def update_task():
+			view_task()
+			try:
+			update=int(input("Please,enter ID task:"))
+			if update in tasks:
+			new_title=input("Please,enter new name task:")
+			new_desc=input("Please,enter new description task")
+			priority=prioryti_def()
+			status=status_def()
+			task[update]={
+				"title":new_title,
+				"desc":new_desc,
+				"priority":priority,
+				"status":status
+			}
+			with open("tasks.txt","w") as file:
+			content=save_task(task)
+			file.write.__str__(content)
+			else:
+			print("Task with ID not find.")
+			except ValueError:
+			print("Please,enter number")
+			except TabError:
+			print("Updated")
 		
 
 
